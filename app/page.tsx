d0d2c722ba174a99212a33a51e9e7825a808bb45
dainamu <katsuya.ominami@gmail.com>
2025-03-23 16:13:19 +0900

    文字の背景色を設定

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
+import "./clockDisp.css";
 
 export default function Home() {
   const [clockDisp, setClockDisp] = useState("00:00:00");
@@ -21,8 +22,17 @@ export default function Home() {
   }, []);
   return (
     <>
-      <h1>{clockDisp}</h1>
-      <h1>{timeZone}</h1>
+      <div className="main ">
+        <div className="gradient"></div>
+      </div>
+      <section className="w-full h-screen flex flex-col justify-center z-10">
+        <div className="flex flex-col justify-center items-center">
+          <div className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 text-9xl font-semibold ">
+            {clockDisp}
+          </div>
+          <div>{timeZone}</div>
+        </div>
+      </section>
     </>
   );
 }
